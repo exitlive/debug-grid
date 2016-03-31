248d49232505caa26adf6e0c71b0830b8233a2e8
Matias Meno <m@tias.me>
2016-03-31 19:34:11 +0200

    refact: first step to get polymer 1.x to work

--- lib/debug_grid.dart
+++ lib/debug_grid.dart
@@ -1,80 +1,84 @@
+@HtmlImport('billing_page.html')
 import 'dart:html';
 
 import 'package:polymer/polymer.dart';
+import 'package:web_components/web_components.dart' show HtmlImport;
+
 import 'package:cookie/cookie.dart' as cookie;
 
-/**
- * Displays a debug grid to verify if all elements are properly aligned.
- *
- * Toggle this debug grid with the `g` shortcut.
- * Toggle the number of columns with the shift key.
- */
-@CustomTag('debug-grid')
-class DebugGridElement extends PolymerElement {
+/// Displays a debug grid to verify if all elements are properly aligned.
+///
+/// Toggle this debug grid with the `g` shortcut.
+/// Toggle the number of columns with the shift key.
+@PolymerRegister('debug-grid')
+class DebugGrid extends PolymerElement {
+  DebugGrid.created() : super.created();
 
   /// This is used to create the names for cookies
   /// If you have multiple debug grids, you should use different
   /// names here
-  @published String name = 'debug-grid';
-
+  @property
+  String name = 'debug-grid';
 
   /// The key that toggles the visibility of the grid.
   /// Use Shift together with it to toggle between 6 and 12 columns
-  @published int toggleKey = KeyCode.G;
+  @property
+  int toggleKey = KeyCode.G;
 
   /// The key that toggles the visibility of the lines.
   /// Use Shift together with this key to toggle the grid instead.
-  @published  int linesToggleKey = KeyCode.L;
-
+  @property
+  int linesToggleKey = KeyCode.L;
 
   /// Whether any of the debug grid is visible.
   /// Can be toggled with [toggleKey]
-  @published bool visible = false;
-
+  @property
+  bool visible = false;
 
   /// Whether to display debug lines
   /// Can be toggled with [linesToggleKey]
-  @published bool showLines = false;
-
+  @property
+  bool showLines = false;
 
   /// Whether to display debug columns.
   /// Can be toggled with Shift - [linesToggleKey]
-  @published bool showColumns = true;
-
+  @property
+  bool showColumns = true;
 
   /// The total width in pixels (without gutters on the outside)
-  @published int totalWidth = 1128;
+  @property
+  int totalWidth = 1128;
 
   /// The gutter width in pixels
-  @published int gutterWidth = 24;
+  @property
+  int gutterWidth = 24;
 
   /// The line height in pixels
-  @published int lineHeight = 24;
-
+  @property
+  int lineHeight = 24;
 
   /// Whether the grid should get smaller if the window size gets smaller
-  @published bool adaptive = true;
-
-
-  @ComputedProperty('100 * gutterWidth / totalWidth')
-  @observable double get gutterWidthPercentage => readValue(#gutterWidthPercentage);
-
-  @ComputedProperty('((totalWidth - (gutterWidth * (columns - 1))) / columns).round()')
-  @observable int get columnWidth => readValue(#columnWidth);
-
-  @ComputedProperty('100 * columnWidth / totalWidth')
-  @observable double get columnWidthPercentage => readValue(#columnWidthPercentage);
-
+  @property
+  bool adaptive = true;
+//
+//  @ComputedProperty('100 * gutterWidth / totalWidth')
+//  @observable
+//  double get gutterWidthPercentage => readValue(#gutterWidthPercentage);
+//
+//  @ComputedProperty('((totalWidth - (gutterWidth * (columns - 1))) / columns).round()')
+//  @observable
+//  int get columnWidth => readValue(#columnWidth);
+//
+//  @ComputedProperty('100 * columnWidth / totalWidth')
+//  @observable
+//  double get columnWidthPercentage => readValue(#columnWidthPercentage);
 
   /// The number of columns to display
-  @published int columns = 6;
+  @property
+  int columns = 6;
 
   /// The cookie name
   static const String COLUMNS_NAME = 'debug-grid-columns';
-
-
-
-  DebugGridElement.created() : super.created();
 
   /**
    * Sets up the key listeners to toggle the grid
@@ -93,7 +97,6 @@ class DebugGridElement extends PolymerElement {
       }
     });
 
-
     String setting;
 
     setting = cookie.get('${name}-visible');
@@ -109,7 +112,6 @@ class DebugGridElement extends PolymerElement {
     if (setting != null) columns = int.parse(setting);
   }
 
-
   /**
    * Small helper function to return a [number] of `<div>`s.
    */
@@ -121,16 +123,14 @@ class DebugGridElement extends PolymerElement {
     return linesHtml;
   }
 
-
   /**
    * Adds all elements needed to show the lines
    */
   attached() {
     var numberOfLines = 100;
-    shadowRoot.querySelector('#lines').setInnerHtml(_getDivs(numberOfLines));
+    $['lines'].setInnerHtml(_getDivs(numberOfLines));
     _setColumnsHtml();
   }
-
 
   /**
    * Invoked whenever [columns] changed and makes sure that the correct amount
@@ -163,8 +163,6 @@ class DebugGridElement extends PolymerElement {
    */
   showLinesChanged() => cookie.set('${name}-show-lines', showLines ? '1' : '0');
 
-
-
   toggleVisibility() {
     print(name);
     visible = !visible;
@@ -180,16 +178,13 @@ class DebugGridElement extends PolymerElement {
     showColumns = true;
   }
 
-
   toggleLines() {
     showColumns = true;
     showLines = !showLines;
   }
+
   toggleColumns() {
     showLines = true;
     showColumns = !showColumns;
   }
-
-
 }
-

58822c9c93c30f8373509f299287db869d134a54
Matias Meno <m@tias.me>
2016-03-31 19:34:29 +0200

    feat: add a test site

---
+++ web/debug_grid.dart
@@ -0,0 +1,11 @@
+library exitlive.webserver.entry_point;
+
+import 'package:polymer/polymer.dart';
+import 'package:debug_grid/debug_grid.dart';
+
+/// Uses [DebugGrid]
+main() {
+
+  initPolymer();
+
+}
